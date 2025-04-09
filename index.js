33142d6836d729d6f748225a27436748c7fdaca1
Himanshu-coder007 <himanshunagose54@gmail.com>
2025-04-09 22:31:30 +0530

    origin updated

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://hr-dashboard-pink.vercel.app/"],
+  origin: "https://hr-dashboard-pink.vercel.app",
   credentials: true,
 };
 app.use(cors(corsOptions));
